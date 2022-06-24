537383edfb266fca46eceee9f86d5339d3fae60b
maimohamed <88830352+mai298@users.noreply.github.com>
2022-06-24 20:53:44 +0200

    Add files via upload

---
+++ DogTestDrive.java
@@ -0,0 +1,17 @@
+public class DogTestDrive {
+    public static void main(String[] args) {
+
+        Dog_Chp_4 one = new Dog_Chp_4();
+        one.size = 70;
+        Dog_Chp_4 two = new Dog_Chp_4();
+        two.size = 8;
+        Dog_Chp_4 three = new Dog_Chp_4();
+        three.size = 35;
+        one.bark();
+        two.bark();
+        three.bark();
+    }
+    }
+
+
+
